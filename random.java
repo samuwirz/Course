55f2aa1faef019bf29f14adf4c5a7d297d1b1b0c
samuwirz <wirzsamuel@hotmail.com>
2015-09-21 11:50:19 -0500

    Update random.java

--- random.java
+++ random.java
@@ -1,6 +1,12 @@
 //Random code
-
 for (int i = 0; i < 10; i++)
 {
 	System.out.println(i);
 }
+
+//Codigo agregado
+String nombre;
+Scanner scan = new Scanner();
+System.out.println("Ingresa tu nombre:");
+nombre = scan.next();
+System.out.println("Hola " + nombre);
